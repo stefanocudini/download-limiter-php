62aade0e48b887b4f3b9fa9b02ec4233fd22a514
z4k <none@none>
2011-08-26 00:10:52 +0200

    aggiunti file di base

---
+++ index.php
@@ -0,0 +1,107 @@
+<?
+#header("Content-type: text/plain");
+/*echo "ciaociao";
+echo "<br>".$_SERVER['QUERY_STRING']."<hr>";
+phpinfo();
+exit(0);*/
+
+$logfile = './downloads.log';
+$countfile = './countdown.log';
+$maxdownload = 3;
+//limite di download per uno stesso file
+
+$dirdown = "/var/www/easyblog.it/download/";
+//directory in cui cercare i file da scaricare!
+//puo cercare anche delle subdirectory di $dirdown! basta specificarlo nel parametro passato allo script
+//da mettere fuori!! della document_root
+
+$target = $_SERVER['QUERY_STRING'];
+//
+////togliere qualunque ../../ si trova in $target prima di scaricare!!!
+//
+
+if(empty($target)):
+?>
+<div style="text-align:center">Area Download</div>
+<?
+else:
+	$name = is_file($dirdown.$target) ? $target : NotFound();
+	$path = $dirdown.$name;
+
+	$cdown = count( array_keys(file($countfile),$target."\n") );
+	
+	if( $cdown >= $maxdownload )
+		 NotFound();
+		 #eliminare il file! e toglierlo da dentro $countfile
+	else
+		$res = sendFile($path);
+		
+	if(!$res['aborted'])
+		@file_put_contents($countfile, $target."\n", FILE_APPEND | LOCK_EX);
+	
+	Logs($res);
+	//forse mettere una exit() dentro Logs() senno continua l'esecuzione del php...
+endif;
+
+
+function NotFound()
+{
+    header('HTTP/1.0 404 Not Found');
+    exit("File non disponibile per il download");
+}
+
+function sendFile($path, $contentType='application/octet-stream')
+{
+	global $target;
+	
+	ignore_user_abort(true);
+	//continua a eseguire php anche se il browser ha stoppato l'esecuzione
+
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Disposition: attachment; filename="'.basename($path) . "\";");
+	header("Content-Transfer-Encoding: binary");	
+	header("Content-Type: $contentType");
+
+	$res = array(
+		'file' => $target,
+		'status' => false,
+		'errors' => array(),
+		'readfileStatus' => null,
+		'aborted' => false
+		);
+
+	$res['readfileStatus'] = readfile($path);
+	
+	if ($res['readfileStatus'] === false)
+	{
+		$res['errors'][] = 'readfile failed.';
+		$res['status'] = false;
+	}
+
+	if (connection_aborted())
+	{
+		$res['errors'][] = 'Connection aborted.';
+		$res['aborted'] = true;
+		$res['status'] = false;
+	}
+
+	return $res;
+}
+
+// Log downloads status
+function Logs($res)
+{
+	global $logfile;
+
+	$abort = $res['aborted']?'ABORT':'FINISH';
+	
+	$info = implode("\t", array($abort, date('[d/m/Y H:i:s]'), $_SERVER['REMOTE_ADDR'], 'File: "'.$res['file'].'"'));
+	//cambiare ordine valori se necessario
+	
+	#touch( $info );
+	
+	@file_put_contents($logfile, $info."\n", FILE_APPEND | LOCK_EX);
+	//LOCK_EX indispensabile senno se tra un download e un altro ce poco tempo non lo scrive sul log!
+}
+
+?>
---
+++ simple.php
@@ -0,0 +1,25 @@
+<?
+
+$dirdown = "/var/www/easyblog.it/download/";
+
+$name = (isset($_SERVER['QUERY_STRING']) and 
+		 is_file($dirdown.$_SERVER['QUERY_STRING'])) ? $_SERVER['QUERY_STRING'] : die("File Not Found");
+
+$path = $dirdown.$name;
+
+ignore_user_abort(true); // Don't end if the connection breaks
+
+header("Content-type: application/octet-stream");
+header("Content-Disposition: attachment; filename=".$name.";" );
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: ".@filesize($path));
+
+if (readfile($path) !== false and !connection_aborted()) {
+// Success!
+	file_put_contents('d.log',"scaricato\n");
+}
+else {
+	file_put_contents('d.log',"annullato\n");
+}
+
+?>
