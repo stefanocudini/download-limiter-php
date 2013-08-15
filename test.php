4f956adf6a85f57e1004ee4fb2b6ad4d64a7be7b
z4k <none@none>
2013-08-15 05:12:33 +0200

    copy

--- test.php
+++ test.php
@@ -3,9 +3,41 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title></title>
+<style>
+body {
+	background:#21214e;
+	color:#ccc
+}
+#copy {
+	position:fixed;
+	z-index:1000;
+	right:140px;
+	top:-6px;
+	font-style:italic;
+	font-size:.85em;
+	padding:5px 8px;
+	background: #3a3a74;
+	border:2px solid #c5cdd4;
+	border-radius:.7em;
+	opacity: 0.8;
+}
+#copy a {
+	color:#ccc;
+}
+#ribbon {
+	position: absolute;
+	top: 0;
+	right: 0;
+	border: 0;
+	filter: alpha(opacity=80);
+	-khtml-opacity: .8;
+	-moz-opacity: .8;
+	opacity: .8;		
+}
+</style>
 </head>
 <body>
-<h3>Area Download</h3>
+<h3>PHP Download Limiter</h3>
 <ol>
 	<li>Scarica <a href="./10MB">questo file vuoto</a> di 10MBytes.</li>
 	<li>Al termine del download ricarica la pagina</li>
@@ -13,7 +45,7 @@
 	<li>Finch&egrave; si annulla il download prima del termine, il contatore non cambia</li>
 	<li>La velocita di download &egrave; sempre limitata a 100 KBytes/s</li>
 </ol>
-<diV style="float:left;border:1px solid #ccc;padding:.5em;">
+<div style="float:left;border:1px solid #ccc;padding:.5em;">
 <? if($remain>0 and $remaintry>0): ?>
 	<i>Puoi effettuare : <br />
 	<b><?=$remain?></b> download completi
@@ -23,6 +55,9 @@
 	<i>Hai terminato il numero massimo di download.</i>
 <? endif; ?>
 </div>
+<div id="copy"><a href="http://labs.easyblog.it/">Labs</a> &bull; <a href="http://labs.easyblog.it/stefano-cudini/">Stefano Cudini</a></div>
+<a href="https://github.com/stefanocudini/jsautocompiler"><img id="ribbon" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+
 <script type="text/javascript" src="/labs-common.js"></script>
 </body>
 </html>
