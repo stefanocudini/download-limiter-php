36d42da3cdaca5bca375ab8b66f23655a8de8c7c
z4k <stefano.cudini@gmail.com>
2014-05-21 02:41:00 +0200

    neglish version

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title></title>
+<title>PHP Download Bandwidth Limiter</title>
 <style>
 body {
 	background:#21214e;
@@ -32,28 +32,27 @@ body {
 	filter: alpha(opacity=80);
 	-khtml-opacity: .8;
 	-moz-opacity: .8;
-	opacity: .8;		
+	opacity: .8;
 }
 </style>
 </head>
 <body>
 <h3>PHP Download Limiter</h3>
 <ol>
-	<li>Scarica <a href="./10MB">questo file vuoto</a> di 10MBytes.</li>
-	<li>Al termine del download ricarica la pagina</li>
-	<li>Scarica di nuovo il file annullando prima del termine</il>
-	<li>Finch&egrave; si annulla il download prima del termine, il contatore non cambia</li>
-	<li>La velocita di download &egrave; sempre limitata a 100 KBytes/s</li>
+	<li>For test <a href="./10MB">download this empty file</a> with size 10MBytes.</li>
+	<li>When the downloade refresh this page.</li>
+	<li>Download the file again canceling before the end.</il>
+	<li>As long as you cancel the download before the end, the counter does not change.</li>
+	<li>The download speed is always limited to 100 KBytes / s </li>
 </ol>
 <div style="float:left;border:1px solid #ccc;padding:.5em;">
-<? if($remain>0 and $remaintry>0): ?>
-	<i>Puoi effettuare : <br />
-	<b><?=$remain?></b> download completi
+<?php if($remain>0 and $remaintry>0): ?>
+	<i>Remain: <b><?=$remain?></b> downloads.</i>
 	<br />
-	<b><?=$remaintry?></b> tentativi di download</i>
-<? else: ?>
-	<i>Hai terminato il numero massimo di download.</i>
-<? endif; ?>
+	<i>Attempts: <b><?=$remaintry?></b> downloads</i>
+<?php else: ?>
+	<i>You have completed the maximum number of downloads.</i>
+<?php endif; ?>
 </div>
 <div id="copy"><a href="http://labs.easyblog.it/">Labs</a> &bull; <a rel="author" href="http://labs.easyblog.it/stefano-cudini/">Stefano Cudini</a></div>
 <a href="https://github.com/stefanocudini/download-limiter-php"><img id="ribbon" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
